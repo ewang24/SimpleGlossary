7c8387a1a4aee391b46d4a406f24842979306fb7
evan <mistix@comcast.net>
2016-08-03 23:38:44 -0600

    Updated current executable jar

--- src/src/Controller/Converter.java
+++ src/src/Controller/Converter.java
@@ -1,26 +1,43 @@
 /**
 Evan Wang
-*/
+ */
 
 package Controller;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Converter
 {
-	public static void main(String [] args)
+	public static void main(String[] args)
 	{
-//		System.out.println("Paste in text to convert");
-//		Scanner s = new Scanner(System.in);
-		String cString = "";
-//	
-//		while(s.hasNext())
-//		{
-//			cString+=s.nextLine()+"\n";
-//		}
-//		System.out.println(cString);
-		
-		String[] s = cString.split("\n");
-		
+
+		try
+		{
+			Scanner s = new Scanner(System.in);
+//			saveWriter.print("v2\r\n");
+			String currentString = "v2\r\n \r\n";
+			String string = "";
+			// Scanner termReader = new Scanner(glossaryFileToUse);
+			BufferedReader termReader = new BufferedReader(new InputStreamReader(new FileInputStream(s.nextLine()),"utf-8"));
+			while ((string = termReader.readLine()) != null)
+			{
+				currentString += string + "::: ::: \r\n";
+			}
+			termReader.close();
+			s.close();
+			System.out.println(currentString);
+			
+			
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
--- src/src/Controller/DataGenerator.java
+++ src/src/Controller/DataGenerator.java
@@ -26,7 +26,8 @@ public class DataGenerator
 
 	public static void main(String[] args)
 	{
-		System.out.println("   ".trim().length());
+		System.out.println("v2".equals("v2"));
+//		System.out.println("   ".trim().length());
 //		System.out.println(" ".trim().equals("") ? true : false);
 //		System.out.println( true&& false &&false );
 		
